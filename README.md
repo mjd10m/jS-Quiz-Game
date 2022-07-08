72b58fe16f1c4803d157bb493ef594e7c09e6808
mjd10m <mjd10m@outlook.com>
2022-07-08 00:32:34 -0400

    README added

---
+++ README.md
@@ -0,0 +1,13 @@
+# JavaScript Quiz Game
+
+## Game Description
+A simple JavaScript quiz game that players the user multiple choice question on basic jS knowledge.  Game is time with 15 seconds being subtracted for each incorrect answer.  At the end of the game the players time remaining is given as the score which they can save to local storage on their device. 
+## Languages Used
+HTML
+CSS
+JavaScript
+## Website Snapshot
+ ![](assets/images/Main-Page.png)
+ ![](assets/images/Highscores-Page.png)
+## Link to Web Page
+https://mjd10m.github.io/jS-Quiz-Game/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,34 @@
-const startButtonE1 = document.getElementById('start-btn')
-const startContainerE1 = document.getElementById('start-container')
-const questionAnswerContainerE1 = document.getElementById('qa-container')
+const startButtonE1 = document.getElementById('start-btn');
+const startContainerE1 = document.getElementById('start-container');
+const questionAnswerContainerE1 = document.getElementById('qa-container');
 let shuffledQuestionsArray, currentQuestion
-const questionE1 = document.getElementById('question')
-const answerE1 = document.getElementById('answers')
-const startingTimeSeconds = 90;
-let currentTime = startingTimeSeconds
-const timerE1 = document.getElementById('timer')
-let timerPaused = true
+const questionE1 = document.getElementById('question');
+const answerE1 = document.getElementById('answers');
+const startingTimeSeconds = 100;
+let currentTime = startingTimeSeconds;
+const timerE1 = document.getElementById('timer');
+let timerPaused = true;
 var timer = setInterval(updateTimer, 1000);
-const gameOverContainerE1 = document.getElementById('game-over-container')
-const submitHighScoreE1 = document.getElementById('save-initials-score')
+const gameOverContainerE1 = document.getElementById('game-over-container');
+const submitHighScoreE1 = document.getElementById('save-initials-score');
 const playAgainE1 = document.getElementById('play-again');
 const homeE1 = document.getElementById('home');
 const highscoresE1 = document.getElementById('highscores');
 var scoreE1 = document.getElementById('score');
 const initialsSubmissionBoxE1 = document.getElementById('initials-form');
 const replayBtns = document.getElementById('replay-btns');
-const updateGameOverMessage = document.getElementById('game-over-main')
-const highscoreBtnE1 = document.getElementById('highscores')
+const updateGameOverMessage = document.getElementById('game-over-main');
+const highscoreBtnE1 = document.getElementById('highscores');
 var highscores = {
     initials: '',
     score: ''
-}
+};
 
-highscoreBtnE1.addEventListener("click", loadHsPage)
-startButtonE1.addEventListener("click", startGame)
-submitHighScoreE1.addEventListener("click", addScore)
-playAgainE1.addEventListener("click", restartGame)
-homeE1.addEventListener("click", returnHome)
-
-function updateTimer() {
-    if (currentTime >= 0){
-        if(!timerPaused){
-        const minutes = Math.floor(currentTime / 60);
-        let seconds = currentTime % 60;
-        if (seconds < 10){
-            timerE1.innerHTML = "Time Remaing: " + minutes + ": " + "0" + seconds;
-        } else {
-            timerE1.innerHTML = "Time Remaing: " + minutes + ": " + seconds;
-        }
-        currentTime--;
-        }
-    } else {
-        console.log('gameover')
-        clearInterval(timer)
-        gamerOver() 
-    }
-
-}
+highscoreBtnE1.addEventListener("click", loadHsPage);
+startButtonE1.addEventListener("click", startGame);
+submitHighScoreE1.addEventListener("click", addScore);
+playAgainE1.addEventListener("click", restartGame);
+homeE1.addEventListener("click", returnHome);
 
 function returnHome() {
     location.reload()
@@ -57,11 +37,11 @@ function startGame() {
     timerPaused = false;
     console.log("Started");
     startContainerE1.classList.add('hide');
-    shuffledQuestionsArray = questions.sort(()=> Math.random() - .5)
-    currentQuestion = 0
-    questionAnswerContainerE1.classList.remove('hide')
-    timerE1.classList.remove('hide')
-    setNextQuestion()
+    shuffledQuestionsArray = questions.sort(()=> Math.random() - .5);
+    currentQuestion = 0;
+    questionAnswerContainerE1.classList.remove('hide');
+    timerE1.classList.remove('hide');
+    setNextQuestion();
 
 }
 
@@ -79,58 +59,54 @@ function setNextQuestion() {
 }
 
 function displayQuestion(question) {
-    questionE1.innerHTML = question.question
+    questionE1.innerHTML = question.question;
     question.answers.forEach(answer => {
         const newButton = document.createElement('button');
         newButton.innerText = answer.text;
         newButton.classList.add('btn');
         if (answer.correct) {
-            newButton.dataset.correct = answer.correct
+            newButton.dataset.correct = answer.correct;
         }
-        newButton.addEventListener('click', selectAnswer)
-        answerE1.appendChild(newButton)
+        newButton.addEventListener('click', selectAnswer);
+        answerE1.appendChild(newButton);
     })
 }
 
 function selectAnswer(event) {
-    const selectedAnswerButton = event.target
-    //const correctAnswer = selectedAnswerButton.dataset.correct
-    isAnswerCorrect(selectedAnswerButton, selectedAnswerButton.dataset.correct)
+    const selectedAnswerButton = event.target;
+    isAnswerCorrect(selectedAnswerButton, selectedAnswerButton.dataset.correct);
     if (shuffledQuestionsArray.length > currentQuestion + 1) {
-        currentQuestion++
-        setTimeout(setNextQuestion, 1500)
+        currentQuestion++;
+        setTimeout(setNextQuestion, 1500);
     } else {
-        clearInterval(timer)
-        console.log('gameover')
-        setTimeout(gameOver, 1500)
+        clearInterval(timer);
+        console.log('gameover');
+        setTimeout(gameOver, 1500);
     }
 
 }
 
 function isAnswerCorrect(selectedAnswerButton, correct) {
-    //clearAnswerCorrect(element)
     const answerMessage = document.getElementById('answer-message');
-    //answerE1.children.classList.remove('correct')
-    //answerE1.childElementCount.classList.remove('incorrect')
     if (correct) {
         console.log('correct');
-        answerMessage.innerText = "Correct"
-        answerMessage.classList.remove('hide')
-        selectedAnswerButton.classList.add('correct')
+        answerMessage.innerText = "Correct";
+        answerMessage.classList.remove('hide');
+        selectedAnswerButton.classList.add('correct');
         timerPaused = true;
     } else {
         console.log('incorrect');
-        answerMessage.innerText = 'Incorrect'
-        answerMessage.classList.remove('hide')
-        selectedAnswerButton.classList.add('incorrect')
-        currentTime = currentTime - 15
+        answerMessage.innerText = 'Incorrect';
+        answerMessage.classList.remove('hide');
+        selectedAnswerButton.classList.add('incorrect');
+        currentTime = currentTime - 15;
         timerPaused = true;
     }
 }
 
 function resetAnswerMessage () {
     const updatedAnswerMessage = document.getElementById('answer-message');
-    updatedAnswerMessage.classList.add('hide')
+    updatedAnswerMessage.classList.add('hide');
 }
 
 
@@ -142,45 +118,75 @@ function resetquestion() {
 }
 
 function gameOver() {
-    
+    if(currentTime > 0) {
     questionAnswerContainerE1.classList.add('hide');
+    timerE1.innerHTML = '';
+    resetAnswerMessage();
+    gameOverContainerE1.classList.remove('hide');
+    updateGameOverMessage.innerHTML = 'Quiz Completed!';
+    scoreE1.innerHTML = "Your score is " + currentTime;
+    } else {
+        questionAnswerContainerE1.classList.add('hide');
     timerE1.innerHTML = ''
     resetAnswerMessage();
     gameOverContainerE1.classList.remove('hide');
-    updateGameOverMessage.innerHTML = 'Quiz Completed!'
-    scoreE1.innerHTML = "Your score is " + currentTime
+    updateGameOverMessage.innerHTML = 'You ran out of time!';
+    scoreE1.innerHTML = "Feel free to try again";
+    initialsSubmissionBoxE1.classList.add('hide');
+    replayBtns.classList.remove('hide');
 
+    }
 }
 
 function addScore(event) {
     event.preventDefault();
     highscores.initials = document.querySelector("input[name='player-initials']").value;
-    highscores.score = currentTime
+    highscores.score = currentTime;
     var currentHighscores = JSON.parse(localStorage.getItem("scores"));
     if (currentHighscores === null) {
-        var newHighscore = []
-        newHighscore.push(highscores)
+        var newHighscore = [];
+        newHighscore.push(highscores);
         localStorage.setItem("scores", JSON.stringify(newHighscore));
     } else {
-        currentHighscores.push(highscores)
+        currentHighscores.push(highscores);
         localStorage.setItem("scores", JSON.stringify(currentHighscores));
     }
     replayBtns.classList.remove('hide');
-    updateGameOverMessage.innerHTML = "Your score has been logged"
-    scoreE1.innerHTML = highscores.initials + " - " + highscores.score
-    initialsSubmissionBoxE1.classList.add('hide')
+    updateGameOverMessage.innerHTML = "Your score has been logged";
+    scoreE1.innerHTML = highscores.initials + " - " + highscores.score;
+    initialsSubmissionBoxE1.classList.add('hide');
 
 }
 
 function restartGame() {
     replayBtns.classList.add('hide');
-    initialsSubmissionBoxE1.classList.remove('hide')
+    initialsSubmissionBoxE1.classList.remove('hide');
     gameOverContainerE1.classList.add('hide');
-    currentTime = startingTimeSeconds
-    timerE1.classList.remove('hide')
+    currentTime = startingTimeSeconds;
+    timerE1.classList.remove('hide');
     timer = setInterval(updateTimer, 1000);
-    startGame()
+    startGame();
     
+
+}
+
+function updateTimer() {
+    if (currentTime >= 0){
+        if(!timerPaused){
+        const minutes = Math.floor(currentTime / 60);
+        let seconds = currentTime % 60;
+        if (seconds < 10){
+            timerE1.innerHTML = "Time Remaing: " + minutes + ": " + "0" + seconds;
+        } else {
+            timerE1.innerHTML = "Time Remaing: " + minutes + ": " + seconds;
+        }
+        currentTime--;
+        }
+    } else {
+        console.log('gameover');
+        clearInterval(timer);
+        gameOver();
+    }
 
 }
 
@@ -189,17 +195,48 @@ function loadHsPage() {
 }
 const questions = [
     {
-        question: 'What is 2 + 2',
+        question: 'Javascript is an _______ language?',
         answers: [
-            {text: '4', correct: true},
-            {text: '45', correct: false}
+            {text: 'Object-Oriented', correct: true},
+            {text: 'Object-Based', correct: false},
+            {text: 'Procedural', correct: false},
+            {text: 'None of the Above', correct: false}
         ]
     },
     {
-        question: 'What is 20 + 25',
+        question: 'Which of the following keywords is used to define a variable in Javascript?',
         answers: [
-            {text: '4', correct: false},
-            {text: '45', correct: true}
+            {text: 'var', correct: false},
+            {text: 'let', correct: false},
+            {text: 'Both A and B', correct: true},
+            {text: 'None of the Above', correct: false}
+        ]
+    },
+    {
+        question: 'Which of the following methods can be used to display data in some form using Javascript?',
+        answers: [
+            {text: 'document.write()', correct: false},
+            {text: 'console.log()', correct: false},
+            {text: 'window.alert()', correct: false},
+            {text: 'All of the Above', correct: true}
+        ]
+    },
+    {
+        question: 'When an operator’s value is NULL, the typeof returned by the unary operator is:',
+        answers: [
+            {text: 'Boolean', correct: false},
+            {text: 'Undefined', correct: false},
+            {text: 'Object', correct: true},
+            {text: 'Integer', correct: false}
+        ]
+    },
+    {
+        question: 'What is the use of the <noscript> tag in Javascript?',
+        answers: [
+            {text: 'The contents are displayed by non-JS-based browsers', correct: true},
+            {text: 'Clears all the cookies and cache', correct: false},
+            {text: 'Both A and B', correct: false},
+            {text: 'None', correct: false}
         ]
     }
 ]
