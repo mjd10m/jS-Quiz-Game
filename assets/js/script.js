26a349b1b0365d381d69ca378dd89abff7b3ec09
mjd10m <mjd10m@outlook.com>
2022-07-07 20:56:02 -0400

    play again and main menu function completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ const highscoresE1 = document.getElementById('highscores');
 var scoreE1 = document.getElementById('score');
 const initialsSubmissionBoxE1 = document.getElementById('initials-form');
 const replayBtns = document.getElementById('replay-btns');
+const updateGameOverMessage = document.getElementById('game-over-main')
 var highscores = {
     initials: '',
     score: ''
@@ -28,10 +29,31 @@ submitHighScoreE1.addEventListener("click", addScore)
 playAgainE1.addEventListener("click", restartGame)
 homeE1.addEventListener("click", returnHome)
 
+function updateTimer() {
+    if (currentTime >= 0){
+        if(!timerPaused){
+        const minutes = Math.floor(currentTime / 60);
+        let seconds = currentTime % 60;
+        if (seconds < 10){
+            timerE1.innerHTML = "Time Remaing: " + minutes + ": " + "0" + seconds;
+        } else {
+            timerE1.innerHTML = "Time Remaing: " + minutes + ": " + seconds;
+        }
+        currentTime--;
+        }
+    } else {
+        console.log('gameover')
+        clearInterval(timer)
+        gamerOver() 
+    }
+
+}
+
 function returnHome() {
     location.reload()
 }
 function startGame() {
+    timerPaused = false;
     console.log("Started");
     startContainerE1.classList.add('hide');
     shuffledQuestionsArray = questions.sort(()=> Math.random() - .5)
@@ -48,8 +70,8 @@ function setNextQuestion() {
         resetquestion();
         displayQuestion(shuffledQuestionsArray[currentQuestion]);
     } else {
-        resetAnswerMessage();
         timerPaused = false;
+        resetAnswerMessage();
         resetquestion();
         displayQuestion(shuffledQuestionsArray[currentQuestion]);
     } 
@@ -118,38 +140,19 @@ function resetquestion() {
     }
 }
 
-function updateTimer() {
-    if (currentTime >= 0){
-        if(!timerPaused){
-        const minutes = Math.floor(currentTime / 60);
-        let seconds = currentTime % 60;
-        if (seconds < 10){
-            timerE1.innerHTML = "Time Remaing: " + minutes + ": " + "0" + seconds;
-        } else {
-            timerE1.innerHTML = "Time Remaing: " + minutes + ": " + seconds;
-        }
-        currentTime--;
-        }
-    } else {
-        console.log('gameover')
-        clearInterval(timer)
-        gamerOver() 
-    }
-
-}
-
 function gameOver() {
+    
     questionAnswerContainerE1.classList.add('hide');
     timerE1.innerHTML = ''
     resetAnswerMessage();
     gameOverContainerE1.classList.remove('hide');
+    updateGameOverMessage.innerHTML = 'Quiz Completed!'
     scoreE1.innerHTML = "Your score is " + currentTime
 
 }
 
 function addScore(event) {
     event.preventDefault();
-    updateGameOverMessage = document.getElementById('game-over-main')
     highscores.initials = document.querySelector("input[name='player-initials']").value;
     highscores.score = currentTime
     var getHighscores = JSON.parse(localStorage.getItem("scores"));
@@ -161,7 +164,6 @@ function addScore(event) {
         savedHighscores.push(highscores);
         localStorage.setItem("scores", JSON.stringify(savedHighscores));
     }
-    debugger;
     replayBtns.classList.remove('hide');
     updateGameOverMessage.innerHTML = "Your score has been logged"
     scoreE1.innerHTML = highscores.initials + " - " + highscores.score
@@ -171,6 +173,7 @@ function addScore(event) {
 
 function restartGame() {
     replayBtns.classList.add('hide');
+    initialsSubmissionBoxE1.classList.remove('hide')
     gameOverContainerE1.classList.add('hide');
     currentTime = startingTimeSeconds
     timerE1.classList.remove('hide')
